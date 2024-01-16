a14fc3ad608d53fbc23fc2d6a59234fa841f03bb
Esternit <92298236+Esternit@users.noreply.github.com>
2024-01-16 17:33:31 +0300

    Added searchbox (sooo cool)

--- scripts/catalog.js
+++ scripts/catalog.js
@@ -1,10 +1,67 @@
-const loading  = document.querySelector('.loader');
-
 
 let limit = 16;
 let page = 1;
+let search = 0;
+let searchValue = "";
+
+function loadSearchHTMLTable(data){
+    const ROOT_PRODUCTS = document.getElementById('listing');
+    if(data.length > 0){
+        let catalog = '';
+    
+        data.forEach(({ img, title, start_price ,id}) => {
+            catalog += `
+            <a class="item" href = "detail.html?id=${id}">
+                <img src="${img}" alt="" class="img">
+                <div class="btn">${title}</div>
+                <div >${start_price} - это цена если что, её тоже желательно оформить</div>
+            </a>
+            `;
+        });
+        const html = `
+        <div class="inner">
+            ${catalog}
+        </div>
+        `;
+        if(page == 1){
+            ROOT_PRODUCTS.innerHTML = html;
+        }
+        else{
+            ROOT_PRODUCTS.innerHTML += html;
+        }
+        
+
+    }
+    }
+function searchfunc(){
+    if(document.querySelector('#search-input') != null){
+        searchValue = document.querySelector('#search-input').value;
+    }
+    
+    if (searchValue.length == 0){
+        page = 1;
+        loader();
+    }
+    else{
+        if(search == 0){
+            page = 1;
+        }
+        search = 1;
+        fetch('https://rmstoreapi-production.up.railway.app/search/' + searchValue, {
+            headers: {
+                'Content-type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify({ limiter : limit, paging : page})
+        })
+        .then(response => response.json())
+        .then(data => loadSearchHTMLTable(data['data']));
+    }
+
+}
 
 function loader (){
+    search = 0;
     fetch('https://rmstoreapi-production.up.railway.app/getAll', {
         headers: {
             'Content-type': 'application/json'
@@ -18,7 +75,9 @@ function loader (){
 
 document.addEventListener('DOMContentLoaded', loader());
 
+
 function loadHTMLTable(data){
+
     if(data.length > 0){
         let catalog = '';
     
@@ -38,7 +97,13 @@ function loadHTMLTable(data){
         `;
     
         const ROOT_PRODUCTS = document.getElementById('listing');
-        ROOT_PRODUCTS.innerHTML += html;
+        if(page == 1){
+            ROOT_PRODUCTS.innerHTML = html;
+        }
+        else{
+            ROOT_PRODUCTS.innerHTML += html;
+        }
+        
     }
     }
 
@@ -67,24 +132,7 @@ function loadHTMLTable(data){
 loader();
  */
 
-function showLoading(){
-    loading.classList.add('show');
 
-    setTimeout(() => {
-        loading.classList.remove('show');
 
-        setTimeout(() => {
-            page++;
-            loader();
-        }, 300);
-    }, 1000);
-}
 
-window.addEventListener('scroll', () => {
-    
-    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-    if (scrollTop + clientHeight >= scrollHeight - 5){
-        
-        showLoading();
-    }
-});
+
---
+++ scripts/postload.js
@@ -0,0 +1,42 @@
+const loading  = document.querySelector('.loader');
+
+function showLoading(){
+    loading.classList.add('show');
+
+    setTimeout(() => {
+        loading.classList.remove('show');
+
+        setTimeout(() => {
+            page++;
+            if (search == 0){
+                loader();
+            }
+            else{
+                searchfunc();
+            }
+            
+        }, 300);
+    }, 1000);
+}
+
+window.addEventListener('scroll', () => {
+    
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight - 5){
+        
+        showLoading();
+    }
+});
+
+function loadsearch(){
+    let searcher = document.getElementById("searcher");
+    let test = document.createElement("div");
+    test.className = "search";
+    test.innerHTML += `<input placeholder="search by name" id="search-input" oninput="searchfunc()">`;
+    searcher.appendChild(test);
+    console.log(test);
+}
+loadsearch();
+
+
+
