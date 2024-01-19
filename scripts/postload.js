69717a046857aed8ff5dc1dd82e2aabd34d0c568
Esternit <92298236+Esternit@users.noreply.github.com>
2024-01-19 17:12:43 +0300

    Update postload.js

--- scripts/postload.js
+++ scripts/postload.js
@@ -22,8 +22,7 @@ function showLoading() {
 window.addEventListener('scroll', () => {
 
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-    console.log(scrollTop, scrollHeight, clientHeight);
-    if (scrollTop + clientHeight >= scrollHeight - 5) {
+    if (scrollTop + clientHeight >= scrollHeight - 20) {
 
         showLoading();
     }
