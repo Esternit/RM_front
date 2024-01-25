e72b26f266a1bd2497f79fc92391274f4263054d
Ignatij3 <ignatijs.krasovskis@gmail.com>
2024-01-25 13:08:10 +0100

    potential fix for ios keyboard issue

--- scripts/catalog.js
+++ scripts/catalog.js
@@ -112,3 +112,14 @@ function loadHTMLTable(data) {
 
     }
 }
+
+function isTextInput(node) {
+    return ['INPUT', 'TEXTAREA'].indexOf(node.nodeName) !== -1;
+}
+
+document.addEventListener('touchstart', function (e) {
+    if (!isTextInput(e.target) && isTextInput(document.activeElement)) {
+        document.activeElement.blur();
+        console.log(document.activeElement);
+    }
+}, false);
