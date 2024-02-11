830e567aa46bca3bc316479f0c68a88632cb490d
Esternit <92298236+Esternit@users.noreply.github.com>
2024-02-11 20:28:22 +0300

    loading animation

--- scripts/postload.js
+++ scripts/postload.js
@@ -30,7 +30,8 @@ function loadsearch() {
     setTimeout(() => {
         document.getElementById("search-input").classList.toggle('transition');
         if (window.location.href.indexOf("search.html") != -1){
-            document.getElementById("search-input").click(document.getElementById("search-input").focus());
+            document.getElementById("search-input").focus();
+            document.getElementById("search-input").click();
         }
     }, 100);
     if (sh != null) {
