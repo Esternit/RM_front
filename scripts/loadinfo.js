61597f522a07c1a7645e7a5d44c3cc2908f12b89
Esternit <92298236+Esternit@users.noreply.github.com>
2024-02-12 14:35:24 +0300

    testing

--- scripts/loadinfo.js
+++ scripts/loadinfo.js
@@ -75,7 +75,8 @@ function loadHTMLTable(data) {
                 pricing: price,
                 size_name: name_size,
                 id: outData[0]["spuId"],
-                img: outData[0]["img"]
+                img: outData[0]["img"],
+                article: outData[0]["article"]
             });
             tg.MainButton.setText("Перейти в чат с менеджером");
             tg.MainButton.show();
