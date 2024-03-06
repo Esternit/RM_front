c63ca88bc5994f6d4a473906d20851030be49ea6
Esternit <92298236+Esternit@users.noreply.github.com>
2024-03-06 18:07:09 +0300

    testing

--- scripts/catalog.js
+++ scripts/catalog.js
@@ -19,7 +19,7 @@ tg.expand();
 tg.MainButton.hide();
 console.log("info");
 const { first_name, last_name, username } = window.Telegram.WebApp.initDataUnsafe.user;
-console.log(first_name, last_name, username );
+console.log(window.Telegram.WebApp.initDataUnsafe);
 function resetsearch() {
     search = 1;
     timescalled++;
